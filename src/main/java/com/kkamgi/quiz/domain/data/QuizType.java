c8148f475c356e90acb4c1f913aee22154d3f237
김승규 <rlatmdrb1996@gmail.com>
2022-07-27 14:05:34 +0900

    Revert "문제집 리스트 api 구현"

--- src/main/java/com/kkamgi/quiz/domain/data/QuizType.java
+++
@@ -1,5 +0,0 @@
-package com.kkamgi.quiz.domain.data;
-
-public enum QuizType {
-    SINGLE_CHOICE, SHORT_ANSWER, LONG_ANSWER
-}
--- src/main/java/com/kkamgi/quiz/repository/QuizbookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kkamgi.quiz.repository;
-
-import com.kkamgi.quiz.domain.Quizbook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizbookRepository extends JpaRepository<Quizbook, Long> {
-
-}
